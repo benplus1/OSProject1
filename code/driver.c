294627859eadd678016494e653eb00873b39202e
Hemanth K. Chiluka <hkc33@prolog.cs.rutgers.edu>
2018-02-09 08:38:26 -0500

    fixing types

--- code/driver.c
+++ code/driver.c
@@ -9,12 +9,14 @@
 
 void my_function(){
 	printf("HELLO THERE\n");
+	while(1==1);
 }
 
 int main(int argc, char ** argv){
 	init();
 	my_pthread_t * thread;
 	my_pthread_create(thread,NULL,(void*)&my_function,NULL);
-
-
+	my_pthread_create(thread,NULL,(void*)&my_function,NULL);
+	
+	//removeThread();
 }
--- code/my_pthread.c
+++ code/my_pthread.c
@@ -8,12 +8,34 @@
 
 #include "my_pthread_t.h"
 
+const int RUNNING=0;
+const int WAITING=1;
+const int READY=2;
+const int LOCKING=3;
+const int TERMINATED=4;
+
+
+
 lq ** scheduler;
 
+int didStart=0;
+struct itimerval timer;
 int mem=4096;
+ucontext_t tailFunc;
+ucontext_t * prevContext;
+void sig_handler(){
+	printf("Caught signal\n");	
+	tcb * next_tcb=(*(scheduler))->front;
+	while(setitimer(ITIMER_VIRTUAL,&timer,NULL)==-1);
+	if(next_tcb!=NULL){
+		ucontext_t * next=next_tcb->context;
+		swapcontext(prevContext, next );
+	}
+}
 
 void init(){
 	printf("Vro\n");
+	//Init scheduler 
 	scheduler=(lq **) malloc(sizeof(lq *)*3);
 	(*(scheduler))=(lq *)malloc(sizeof(lq));	
 	(*(scheduler))->front=NULL;
@@ -21,18 +43,39 @@ void init(){
 	(*(scheduler+1))->front=NULL;
 	(*(scheduler+2))=(lq *)malloc(sizeof(lq));	
 	(*(scheduler+2))->front=NULL;
+	
+	prevContext=malloc(sizeof(ucontext_t));
+	getcontext(prevContext);
+		
+	//Init tail context for every thread
+	getcontext(&tailFunc);
+	tailFunc.uc_link=0;
+	tailFunc.uc_stack.ss_sp=malloc(mem);
+	tailFunc.uc_stack.ss_size=mem;
+	tailFunc.uc_stack.ss_flags=0;
+	makecontext(&tailFunc,(void *)&my_pthread_create, 0);
+	
+	//init alarm
+	while(signal(SIGVTALRM,(void *)&sig_handler)==SIG_ERR);
+	timer.it_value.tv_sec=25/1000;
+	timer.it_value.tv_usec=25;
+	timer.it_interval=timer.it_value;
+	while(setitimer(ITIMER_VIRTUAL,&timer,NULL)==-1);
+	printf("Done init\n");	
+	//while(1==1);
 }
 
 ucontext_t * init_context(void* func){
 	ucontext_t * t=(ucontext_t *) malloc(sizeof(ucontext_t));
 	getcontext(t);
-	t->uc_link=0;
+	t->uc_link=&tailFunc;
 	t->uc_stack.ss_sp=malloc(mem);
 	t->uc_stack.ss_size=mem;
 	t->uc_stack.ss_flags=0;
 	makecontext(t,func, 0);
 	return t;
 }
+
 
 tcb * init_tcb(ucontext_t * context, my_pthread_t * tid){
 	tcb * curr=(tcb *)malloc(sizeof(tcb *));
@@ -45,7 +88,7 @@ void enqueue(tcb * curr, int priority){
 	lq * currlq =scheduler[priority];
 	tcb * ptr=currlq->front;
 	if(ptr==NULL){
-		ptr=curr;
+		currlq->front=curr;
 	}else{
 		while(ptr->right!=NULL){
 			ptr=ptr->right;
@@ -56,13 +99,30 @@ void enqueue(tcb * curr, int priority){
 	printf("Enqueued\n");
 }
 
+void removeThread(tcb * curr){
+	tcb * left=curr->left;
+	tcb * right=curr->right;
+	
+	left->right=right;
+	if(right!=NULL){
+		right->left=left;
+	}	
+	curr->state=TERMINATED;	
+}
+
+
+
 
 /* create a new thread */
 int my_pthread_create(my_pthread_t * thread, pthread_attr_t * attr, void *(*function)(void*), void * arg) {
 	ucontext_t * my_context = init_context(function);
 	//setcontext(&my_context);
 	tcb * curr= init_tcb(my_context, thread);
-	enqueue(curr, 0);	
+	enqueue(curr, 0);
+	if(!didStart){
+		didStart=1;
+		sig_handler();
+	}	
 	return 0;
 };
 
